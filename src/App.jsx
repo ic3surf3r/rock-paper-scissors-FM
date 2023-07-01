abf6068c262dc52a5f452a475838f2020b849a0b
ic3surf3r <novis.joao@gmail.com>
2023-07-01 14:40:52 +0200

    rules position on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Grid from "./components/Grid";
 import Header from "./components/Header";
+import Rules from "./components/Rules";
 
 function App() {
   const [score, setScore] = useState(0);
@@ -14,23 +15,28 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col text-center text-dark-Text-Color h-screen w-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-background-Start-Color to-background-end-Color p-10">
-      <Header score={score} />
-      <div className="flex space-x-3 mx-auto">
-        <button
-          className="bg-white rounded-md w-8 h-8 m-3"
-          onClick={increaseScore}
-        >
-          +1
-        </button>
-        <button
-          className="bg-white rounded-md w-12 h-8 m-3"
-          onClick={clearScore}
-        >
-          Clear
-        </button>
+    <div className="relative text-dark-Text-Color h-screen w-screen bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-background-Start-Color to-background-end-Color p-10">
+      <div className="flex flex-col text-center ">
+        <Header score={score} />
+        <div className="flex space-x-3 mx-auto">
+          <button
+            className="bg-white rounded-md w-8 h-8 m-3"
+            onClick={increaseScore}
+          >
+            +1
+          </button>
+          <button
+            className="bg-white rounded-md w-12 h-8 m-3"
+            onClick={clearScore}
+          >
+            Clear
+          </button>
+        </div>
+        <Grid />
       </div>
-      <Grid />
+      <div className="absolute bottom-8 right-8">
+        <Rules />
+      </div>
     </div>
   );
 }
