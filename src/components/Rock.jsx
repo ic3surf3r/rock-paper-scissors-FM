7b6eb0c030f80ac85c44e3da2ae2a0595fea5432
ic3surf3r <novis.joao@gmail.com>
2023-07-04 23:19:19 +0200

    proto game function done

--- src/components/Rock.jsx
+++ src/components/Rock.jsx
@@ -1,44 +1,14 @@
-import { data } from "autoprefixer";
 import rockIcon from "../assets/icon-rock.svg";
+import { playGame } from "../functions/playGame";
 
 function Rock() {
-  const getComputerChoice = () => {
-    const choices = ["rock", "paper", "scissors"];
-    const randomIndex = Math.floor(Math.random() * choices.length);
-    return choices[randomIndex];
-  };
-
-  const playRock = () => {
-    const data = {
-      pChoice: "rock",
-      cChoice: getComputerChoice(),
-      winner: "",
-    };
-
-    const { pChoice, cChoice } = data;
-
-    if (pChoice === cChoice) {
-      data.winner = "It's a tie!";
-    } else if (
-      (pChoice === "rock" && cChoice === "scissors") ||
-      (pChoice === "paper" && cChoice === "rock") ||
-      (pChoice === "scissors" && cChoice === "paper")
-    ) {
-      data.winner = "Player Wins!";
-    } else {
-      data.winner = "Computer Wins!";
-    }
-
-    console.log(data);
-  };
-
   return (
     <div className="flex items-center justify-center">
       <div className="relative">
         <div
           className="relative rounded-full bg-gradient-to-b from-rock-start to-rock-end w-44 h-44
       flex items-center justify-center z-20 cursor-pointer"
-          onClick={playRock}
+          onClick={() => playGame("rock")}
         >
           <div
             className="absolute rounded-full bg-white w-36 h-36
---
+++ src/functions/playGame.js
@@ -0,0 +1,31 @@
+export function playGame(input) {
+  const data = {
+    pChoice: input,
+    cChoice: getComputerChoice(),
+    winner: "",
+  };
+
+  const { pChoice, cChoice } = data;
+
+  if (pChoice === cChoice) {
+    data.winner = "It's a tie!";
+  } else if (
+    (pChoice === "rock" && cChoice === "scissors") ||
+    (pChoice === "paper" && cChoice === "rock") ||
+    (pChoice === "scissors" && cChoice === "paper")
+  ) {
+    data.winner = "Player Wins!";
+  } else {
+    data.winner = "Computer Wins!";
+  }
+
+  console.log(data);
+
+  return data;
+}
+
+const getComputerChoice = () => {
+  const choices = ["rock", "paper", "scissors"];
+  const randomIndex = Math.floor(Math.random() * choices.length);
+  return choices[randomIndex];
+};
