09680ed1ed913ee5854bc1d43ed2091235513282
ic3surf3r <novis.joao@gmail.com>
2023-07-07 18:53:58 +0200

    header for mobile done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,9 +9,9 @@ function Header() {
   return (
     <div
       className="flex justify-between rounded-lg border-4 border-solid border-header-Outline-Color w-10/12 lg:w-5/12
-    mx-auto py-5 px-6 mb-6 "
+    mx-auto py-3 px-6 mb-6 "
     >
-      <img src={logo} alt="Rock Paper Scissors" />
+      <img src={logo} alt="Rock Paper Scissors" className="w-2/5 md:w-auto" />
       <Score score={score} />
     </div>
   );
--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -1,10 +1,12 @@
 function Score({ score }) {
   return (
-    <div className="flex flex-col justify-center space-y-0 text-center bg-white rounded-lg w-24">
-      <div className="text-score-Text-Color font-body text-sm m-0 p-0">
+    <div className="flex flex-col justify-center space-y-0 text-center bg-white rounded-lg w-16 md:w-24 h-auto">
+      <div className="text-score-Text-Color font-body text-xs md:text-sm m-0 p-0">
         SCORE
       </div>
-      <div className="font-body font-bold text-6xl m-0 p-0">{score}</div>
+      <div className="font-body font-bold text-xl md:text-6xl m-0 p-0">
+        {score}
+      </div>
     </div>
   );
 }
